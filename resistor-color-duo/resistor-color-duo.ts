f7dc14ac2113b5ed8344fc2d4af2c747cb7ff7db
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-02-16 21:08:12 +0000

    Refactor to use array instead of map

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,30 +1,27 @@
 export class ResistorColor {
     private readonly colors: string[]
-    private colorToValue = new Map([
-        ['black', 0],
-        ['brown', 1],
-        ['red', 2],
-        ['orange', 3],
-        ['yellow', 4],
-        ['green', 5],
-        ['blue', 6],
-        ['violet', 7],
-        ['grey', 8],
-        ['white', 9]
-    ]);
+    private colorMap = [
+        'black',
+        'brown',
+        'red',
+        'orange',
+        'yellow',
+        'green',
+        'blue',
+        'violet',
+        'grey',
+        'white'
+    ];
 
     constructor(colors: string[]) {
-        this.colors = colors
-        const notEnoughColors = this.colors.length < 2;
+        const notEnoughColors = colors.length < 2;
         if (notEnoughColors) throw new Error('At least two colors need to be present');
+        this.colors = colors;
     }
 
-    private readonly INVALID_VALUES = 99;
     value = (): number => {
-        const firstResistor = this.colorToValue.get(this.colors[0]);
-        const secondResistor = this.colorToValue.get(this.colors[1]);
-        const colorsAreValid = firstResistor != undefined && secondResistor != undefined;
-        if (colorsAreValid) return +`${firstResistor}${secondResistor}`;
-        return this.INVALID_VALUES;
+        const firstResistor = this.colorMap.indexOf(this.colors[0]);
+        const secondResistor = this.colorMap.indexOf(this.colors[1]);
+        return Number(`${firstResistor}${secondResistor}`);
     }
 }
