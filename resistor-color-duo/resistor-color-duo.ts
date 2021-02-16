179c70642956ac68bc065d188d16beb87c1b1abc
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-02-16 21:02:13 +0000

    Complete resistor color exercise

--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,30 @@
 export class ResistorColor {
-  private colors: string[]
+    private readonly colors: string[]
+    private colorToValue = new Map([
+        ['black', 0],
+        ['brown', 1],
+        ['red', 2],
+        ['orange', 3],
+        ['yellow', 4],
+        ['green', 5],
+        ['blue', 6],
+        ['violet', 7],
+        ['grey', 8],
+        ['white', 9]
+    ]);
 
-  constructor(colors: string[]) {
-    this.colors = colors
-  }
-  value = (): number => 0
+    constructor(colors: string[]) {
+        this.colors = colors
+        const notEnoughColors = this.colors.length < 2;
+        if (notEnoughColors) throw new Error('At least two colors need to be present');
+    }
+
+    private readonly INVALID_VALUES = 99;
+    value = (): number => {
+        const firstResistor = this.colorToValue.get(this.colors[0]);
+        const secondResistor = this.colorToValue.get(this.colors[1]);
+        const colorsAreValid = firstResistor != undefined && secondResistor != undefined;
+        if (colorsAreValid) return +`${firstResistor}${secondResistor}`;
+        return this.INVALID_VALUES;
+    }
 }
