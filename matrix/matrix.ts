41f47695373c6f7283a6aba3429ed850a6615fd0
Khoovi Seewoonauth <khoovirajsingh.seewoonauth@codurance.com>
2021-02-19 08:57:56 +0000

    Complete matrix exercise

--- matrix/matrix.ts
+++ matrix/matrix.ts
@@ -1,10 +1,32 @@
 class Matrix {
-  rows = [[1]];
-  columns = [[1]];
+    rows: number[][] = [];
+    columns: number[][] = [];
+    private lines: string[];
 
-  constructor(public number: string) {
-    // Your code here
-  }
+    constructor(public matrix: string) {
+        this.lines = matrix.split('\n');
+        this.generateMatrix();
+    }
+
+    private generateMatrix() {
+        this.generateRows();
+        this.generateColumns();
+    }
+
+    private generateRows() {
+        const toRows = (line: string) => line.split(' ')
+            .map(character => Number(character));
+        this.rows = this.lines.map(toRows);
+    }
+
+    private generateColumns() {
+        for (let i = 0; i < this.rows[0].length; i++) {
+            this.columns.push([]);
+            for (const row of this.rows) {
+                this.columns[i].push(row[i]);
+            }
+        }
+    }
 }
 
 export default Matrix
